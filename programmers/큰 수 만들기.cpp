cc1fadd6a26ed528726446f21efed5d5d2fc0856
Kim Minjun <junjun612@naver.com>
2020-12-06 00:50:31 +0900

    Create 큰 수 만들기.cpp

---
+++ programmers/큰 수 만들기.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string number, int k) {
+	for (int i = 0; i < number.length() - 1;)
+	{
+		if (0 == k)
+			break;
+
+		if (number[i] >= number[i + 1])
+		{
+			i++;
+			continue;
+		}
+
+		number = number.substr(0, i) + number.substr(i + 1);
+		k--;
+		i = max(0, i - 1);
+	}
+
+	return number.substr(0, number.length() - k);
+}
