dcb49faac711ef759bdbb56d5570147e87acf28a
Kim Minjun <junjun612@naver.com>
2020-12-06 16:39:18 +0900

    Create fibonacci.cpp

---
+++ dp/fibonacci.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[100001] = {0, };
+
+int fibonacci(int n) {
+	if (n <= 1)
+		return n;
+
+	if (dp[n] != 0)
+		return dp[n];
+
+	dp[n] = fibonacci(n - 1) + fibonacci(n - 2);
+	return dp[n];
+}
+
+int main() {
+	cout << fibonacci(40) << endl;
+
+	return 0;
+}
